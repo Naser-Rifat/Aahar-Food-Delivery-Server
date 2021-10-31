8803ae59c9a59ade83bb86372d75bf81a10bc974
Naser Rifat <39612363+Naser-Rifat@users.noreply.github.com>
2021-10-31 22:03:21 +0600

    add

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ const ObjectId = require("mongodb").ObjectId;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g0xoz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+console.log(uri);
+
 
 
 async function run() {
